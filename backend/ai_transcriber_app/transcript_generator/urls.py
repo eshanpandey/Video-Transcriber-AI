d6907a3c680d4cb97907c2aed5760388b26f6e70
Eshan Pandey <pandeyeshan13@gmail.com>
2024-04-04 00:23:35 +0530

    wahahaha

--- backend/ai_transcriber_app/transcript_generator/urls.py
+++ backend/ai_transcriber_app/transcript_generator/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
    path('', views.index, name='index'),
+   path('login', views.login, name='login'),
+   path('signup',views.signup, name='signup'),
+   path('allscripts',views.all_scripts, name='all-scripts'),
+
 ]
--- backend/ai_transcriber_app/transcript_generator/views.py
+++ backend/ai_transcriber_app/transcript_generator/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
 
+def login(request):
+    return render(request,'login.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def all_scripts(request):
+    return render(request,'all-scripts.html')
+
