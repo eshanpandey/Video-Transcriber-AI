0c5b1cf7c2f88070544d24da6e1778db2132c65a
Eshan Pandey <pandeyeshan13@gmail.com>
2024-04-04 02:27:43 +0530

    made some changes

--- backend/ai_transcriber_app/transcript_generator/urls.py
+++ backend/ai_transcriber_app/transcript_generator/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
    path('', views.index, name='index'),
-   path('login', views.login, name='login'),
-   path('signup',views.signup, name='signup'),
+   path('login', views.user_login, name='login'),
+   path('signup',views.user_signup, name='signup'),
+   path('logout',views.logout, name='logout'),
    path('allscripts',views.all_scripts, name='all-scripts'),
 
 ]
--- backend/ai_transcriber_app/transcript_generator/views.py
+++ backend/ai_transcriber_app/transcript_generator/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
-def login(request):
+def user_login(request):
     return render(request,'login.html')
 
-def signup(request):
-    return render(request,'signup.html')
+def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        repeatPassword = request.POST['repeatPassword']
+
+        if password == repeatPassword:
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                login(request, user)
+                return redirect('/')
+            except:
+                error_message = 'Error creating account'
+                return render(request, 'signup.html', {'error_message':error_message})
+        else:
+            error_message = 'Password do not match'
+            return render(request, 'signup.html', {'error_message':error_message})
+        
+    return render(request, 'signup.html')
 
 def all_scripts(request):
     return render(request,'all-scripts.html')
+
+def user_logout(request):
+    return render(request,'logout.html')
 
