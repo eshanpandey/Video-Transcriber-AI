bee464e521bef34b37b239f1841f6f6b779837b3
Eshan Pandey <pandeyeshan13@gmail.com>
2024-04-12 19:59:03 +0530

    new changes

--- backend/ai_transcriber_app/transcript_generator/models.py
+++ backend/ai_transcriber_app/transcript_generator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class ArticlePost(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)#user who created the article
+    video_title=models.CharField(max_length=300)
+    video_link = models.URLField(max_length=300)
+    generated_content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.video_title
